200f8574c3e7325fa5b517818bbdf45e474275ff
AbiralTr <abiraltuladhar22@gmail.com>
2023-11-10 13:38:36 -0700

    updated rock paper scissors + lower method

--- homework/7_homework/CandyLand/Player.cpp
+++ homework/7_homework/CandyLand/Player.cpp
@@ -1,5 +1,7 @@
 #include "Player.h"
 #include <iostream>
+#include <string>
+#include <iomanip>
 
 using namespace std;
 
@@ -33,12 +35,20 @@ void Player::printInventory(){
     std::cout << "|[" << _inventory[2].name << "]|[" << _inventory[3].name << "]|" << std::endl; 
 }
 
+string lower(string s){
+    string lowered_string = s;
+    for(int i = 0; i < s.length(); i++){
+        lowered_string[i] = tolower(lowered_string[i]);
+    }
+    return lowered_string;
+}
+
 Candy Player::findCandy(std::string candy_name){
     Candy foundCandy{""};
     bool found = false;
     for(int i = 0; i < 4; i ++){
         if(!found){
-            if(_inventory[i].name == candy_name){
+            if(lower(_inventory[i].name) == lower(candy_name)){
                 found = true;
                 foundCandy = _inventory[i];
             }
@@ -104,41 +114,142 @@ std::string Player::getEffect(){
 }
 
 void playRockPaperScissors(Player players[2]){
-    Candy player1bet;
-    Candy player2bet;
-    bool running = true;
-    while(running){
-        string bet = "";
+    if(players[0].getCandyAmount() == 0 || players[1].getCandyAmount() == 0){
+        cout << "Not enough candy!" << endl;
+    } else{
+        Candy player1bet;
+        Candy player2bet;
+        bool running = true;
+        // Player 1 and 2 Setting their bets
+        string player1betname = "";
+        string player2betname = "";
         cout << "Player 1 Inventory" << endl;
         players[0].printInventory();
         bool findingCandy = true;
         while(findingCandy){
             cout << "Player 1: Select candy to bet" << endl;
-            getline(cin, bet);
-            player1bet = players[0].findCandy(bet);
+            getline(cin, player1betname);
+            player1bet = players[0].findCandy(player1betname);
             if(player1bet.name == ""){
                 cout << "Invalid selection!" << endl;
+                bool loop = true;
+                while(loop){
+                    getline(cin, player2betname);
+                    player2bet = players[1].findCandy(player2betname);
+                    if(player2bet.name != ""){
+                        findingCandy = false;
+                        loop = false;
+                    }
+                }
             } else{
                 findingCandy = false;
             }
         }
 
-        bet = "";
         cout << "Player 2 Inventory" << endl;
         players[1].printInventory();
         findingCandy = true;
         while(findingCandy){
             cout << "Player 2: Select candy to bet" << endl;
-            getline(cin, bet);
-            player2bet = players[1].findCandy(bet);
+            getline(cin, player2betname);
+            player2bet = players[1].findCandy(player2betname);
             if(player2bet.name == ""){
                 cout << "Invalid selection!" << endl;
+                bool loop = true;
+                while(loop){
+                    getline(cin, player2betname);
+                    player2bet = players[1].findCandy(player2betname);
+                    if(player2bet.name != ""){
+                        findingCandy = false;
+                        loop = false;
+                    }
+                }
             } else{
                 findingCandy = false;
             }
         }
-        
+        // Game Starts
+        char player1selection;
+        char player2selection;
+        while(running){
+            bool selecting = true;
+            while(selecting){
+                cout << "Player 1: Enter r, p, or s" << endl;
+                cin >> player1selection;
+                if(player1selection == 'r' || player1selection == 'p' || player1selection == 's'){
+                    selecting = false;
+                } else{
+                    cout << "Invalid selection!" << endl;
+                    bool loop = true;
+                    while(loop){
+                        cin >> player1selection;
+                        if(player1selection == 'r' || player1selection == 'p' || player1selection == 's'){
+                            selecting = false;
+                            loop = false;
+                        }
+                    }
+                }
+            }
+            selecting = true;
+            while(selecting){
+                cout << "Player 2: Enter r, p, or s" << endl;
+                cin >> player2selection;
+                if(player2selection == 'r' || player2selection == 'p' || player2selection == 's'){
+                    selecting = false;
+                } else{
+                    cout << "Invalid selection!" << endl;
+                    bool loop = true;
+                    while(loop){
+                        cin >> player1selection;
+                        if(player1selection == 'r' || player1selection == 'p' || player1selection == 's'){
+                            selecting = false;
+                            loop = false;
+                        }
+                    }
+                }
+            }
+
+            if(player1selection != player2selection){
+                running = false;
+            } else{
+                cout << "Tie! Play again" << endl;
+            }
+            
+        }
+
+        if(player1selection == 'r'){
+            if(player2selection == 's'){
+                cout << "Player 1 has won " << player2betname << " from player 2!" << endl;
+                players[0].addCandy(player2bet);
+                players[1].removeCandy(player2bet.name);
+            } else if(player2selection == 'p'){
+                cout << "Player 2 has won " << player1betname << " from player 1!" << endl;
+                players[1].addCandy(player1bet);
+                players[0].removeCandy(player1bet.name);
+            }
+        } else if(player1selection == 'p'){
+            if(player2selection == 'r'){
+                cout << "Player 1 has won " << player2betname << " from player 2!" << endl;
+                players[0].addCandy(player2bet);
+                players[1].removeCandy(player2bet.name);
+            } else if(player2selection == 's'){
+                cout << "Player 2 has won " << player1betname << " from player 1!" << endl;
+                players[1].addCandy(player1bet);
+                players[0].removeCandy(player1bet.name);
+            }
+        } else if(player1selection == 's'){
+            if(player2selection == 'p'){
+                cout << "Player 1 has won " << player2betname << " from player 2!" << endl;
+                players[0].addCandy(player2bet);
+                players[1].removeCandy(player2bet.name);
+            } else if(player2selection == 'r'){
+                cout << "Player 2 has won " << player1betname << " from player 1!" << endl;
+                players[1].addCandy(player1bet);
+                players[0].removeCandy(player1bet.name);
+            }
+        }
     }
+    
 }
 
 int main(){
