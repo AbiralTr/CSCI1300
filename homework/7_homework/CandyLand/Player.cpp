6b542b36c84b3af48cdaa7622ff9cc31f0c72e7f
AbiralTr <abiraltuladhar22@gmail.com>
2023-11-10 17:42:18 -0700

    updated 9

--- homework/7_homework/CandyLand/Player.cpp
+++ homework/7_homework/CandyLand/Player.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
@@ -265,14 +267,328 @@ void playRockPaperScissors(Player players[2]){
     
 }
 
-int main(){
+vector<Candy> readCandy(string file_name, vector<Candy> candies){
+    string name;
+    string description;
+    double price = 0.0;
+    string type;
+
+    ifstream file(file_name);
+    if(file.fail()){
+        cout << "Failed to open file" << endl;
+        return candies;
+    }
     
+    string line;
+    while(getline(file, line)){
+        if(line.length() == 0){
+            continue;
+        }
+        Candy current_candy = Candy();
+        string word;
+        stringstream ss(line);
+        vector<string> stats;
+        while(getline(ss, word, '|')){
+            stats.push_back(word);
+        }
+
+        current_candy.name = stats.at(0);
+        current_candy.description = stats.at(1);
+        current_candy.price = stod(stats.at(2));
+        current_candy.candy_type = stats.at(3);
+        
+        candies.push_back(current_candy);
+    }
+
+    return candies;
+}
+
+void displayCandyVector(vector<Candy> candies){
+    for(int i = 0; i < candies.size(); i++){
+        cout << "Name: " << candies.at(i).name << ". Description: " << candies.at(i).description << ". Price: " << fixed << setprecision(2) << candies.at(i).price << ". " << "Type: " << candies.at(i).candy_type << endl;
+    }
+}
+
+int main(){
+    Player player1 = Player();
+    Player player2 = Player();
+    Player players[2] = {player1, player2};
+
+    bool running = true;
     cout << "Select from the following options:" << endl;
     cout << "1. Load Candy" << endl;
-    cout << "1. Load Candy" << endl;
-    cout << "1. Load Candy" << endl;
-    cout << "1. Load Candy" << endl;
-    cout << "1. Load Candy" << endl;
+    cout << "2. Display player stats" << endl;
+    cout << "3. Edit player stats" << endl;
+    cout << "4. play rock-paper-scissors" << endl;
+    cout << "5. Exit" << endl;
 
+    string input_string;
+    int input = 0;
+    vector<Candy> availableCandy;
+    string file_name;
+    bool loop = false;
+    bool valid = false;
+
+    while(running){
+        
+        cin >> input;
+        switch(input){
+            case 1:
+                cout << "Enter file to read:" << endl;
+                cin >> file_name;
+                readCandy(file_name, availableCandy);
+                cout << "Select from the following options:" << endl;
+                cout << "1. Load Candy" << endl;
+                cout << "2. Display player stats" << endl;
+                cout << "3. Edit player stats" << endl;
+                cout << "4. play rock-paper-scissors" << endl;
+                cout << "5. Exit" << endl;
+                break;
+            case 2:
+                cout << "Which player's stats do you want to display? Choose 1 or 2:" << endl;
+                cin >> input; // Might break (using same input var)
+                switch(input){
+                    case 1:
+                        cout << "Player 1 stats:" << endl;
+                        cout << "Stamina: " << player1.getStamina() << endl;
+                        cout << "Gold: " << fixed << setprecision(2) << player1.getGold() << endl;
+                        cout << "Effect: " << player1.getEffect() << endl;
+                        player1.printInventory();
+                        break;
+                    case 2:
+                        cout << "Player 2 stats:" << endl;
+                        cout << "Stamina: " << player2.getStamina() << endl;
+                        cout << "Gold: " << fixed << setprecision(2) << player2.getGold() << endl;
+                        cout << "Effect: " << player2.getEffect() << endl;
+                        player2.printInventory();
+                        break;
+                    default:
+                        cout << "Invalid input!" << endl;
+                        break;
+                }
+                cout << "Select from the following options:" << endl;
+                cout << "1. Load Candy" << endl;
+                cout << "2. Display player stats" << endl;
+                cout << "3. Edit player stats" << endl;
+                cout << "4. play rock-paper-scissors" << endl;
+                cout << "5. Exit" << endl;
+                break;
+            case 3:
+                cout << "Which player's stats do you want to edit? Choose 1 or 2:" << endl;
+                cin >> input; // Might break (using same input var)
+                switch(input){
+                    case 1: // Editting Player 1 Stats
+                        cout << "Editing Player 1 stats:" << endl;
+                        cout << "1. Edit Stamina" << endl;
+                        cout << "2. Edit Gold" << endl;
+                        cout << "3. Edit Effect" << endl;
+                        cout << "4. Add Candy" << endl;
+                        cout << "5. Remove Candy" << endl;
+                        cin >> input;
+                        switch(input){
+                            case 1:
+                                cout << "Editing stamina. Enter a value between 1 and 10:" << endl;
+                                cin >> input;
+                                if(input > 0 && input < 11){
+                                    player1.setStamina(input);
+                                } else{
+                                    loop = true;
+                                    while(loop){
+                                        cout << "Invalid input!" << endl;
+                                        cin >> input;
+                                        if(input > 0 && input < 11){
+                                            loop = false;
+                                            player1.setStamina(input);
+                                        }
+                                    }
+                                    
+                                }
+                                break;
+                            case 2:
+                                cout << "Editing gold. Enter a value between 0 and 1000:" << endl;
+                                cin >> input;
+                                if(input > 0 && input < 1000.1){
+                                    player1.setGold(input);
+                                } else{
+                                    loop = true;
+                                    while(loop){
+                                        cout << "Invalid input!" << endl;
+                                        cin >> input;
+                                        if(input > -1 && input < 1000.1){
+                                            loop = false;
+                                            player1.setGold(input);
+                                        }
+                                    }
+                                    
+                                }
+                                break;
+                            case 3:
+                                cout << "Editing Effect. Enter the name of an effect as a string" << endl;
+                                cin >> input_string;
+                                player1.setEffect(input_string);
+                                break;
+                            case 4:
+                                if(availableCandy.size() != 0){
+                                    cout << "Adding candy. Enter one of the following options:" << endl;
+                                    displayCandyVector(availableCandy);
+                                    getline(cin, input_string);
+                                    for(int i = 0; i < availableCandy.size(); i++){
+                                        if(valid){
+                                            continue;
+                                        } else{
+                                            if(availableCandy.at(i).name == lower(input_string)){
+                                                valid = true;
+                                                player1.addCandy(availableCandy.at(i));
+                                            }
+                                        }
+                                    }
+                                } else{
+                                    cout << "Cannot add candy! Read a candy file first" << endl;
+                                }
+                                break;
+                            case 5:
+                                cout << "Removing Candy. Enter the name of a candy in player 1's inventory:" << endl;
+                                player1.printInventory();
+                                getline(cin, input_string);
+                                if(!player1.removeCandy(input_string)){
+                                    loop = true;
+                                    while(loop){
+                                        cout << "Invalid input!" << endl;
+                                        getline(cin, input_string);
+                                        if(player1.removeCandy(input_string)){
+                                            loop = false;
+                                        }
+                                    }
+                                }
+                                break;
+                            default:
+                                cout << "Invalid input!" << endl;
+                                break;
+                        }
+                        break;
+                    case 2: // Editting Player 2 Stats
+                        cout << "Editing Player 2 stats:" << endl;
+                        cout << "1. Edit Stamina" << endl;
+                        cout << "2. Edit Gold" << endl;
+                        cout << "3. Edit Effect" << endl;
+                        cout << "4. Add Candy" << endl;
+                        cout << "5. Remove Candy" << endl;
+                        cin >> input;
+                        switch(input){
+                            case 1:
+                                cout << "Editing stamina. Enter a value between 1 and 10:" << endl;
+                                cin >> input;
+                                if(input > 0 && input < 11){
+                                    player2.setStamina(input);
+                                } else{
+                                    loop = true;
+                                    while(loop){
+                                        cout << "Invalid input!" << endl;
+                                        cin >> input;
+                                        if(input > 0 && input < 11){
+                                            loop = false;
+                                            player2.setStamina(input);
+                                        }
+                                    }
+                                    
+                                }
+                                break;
+                            case 2:
+                                cout << "Editing gold. Enter a value between 0 and 1000:" << endl;
+                                cin >> input;
+                                if(input > 0 && input < 1000.1){
+                                    player2.setGold(input);
+                                } else{
+                                    loop = true;
+                                    while(loop){
+                                        cout << "Invalid input!" << endl;
+                                        cin >> input;
+                                        if(input > -1 && input < 1000.1){
+                                            loop = false;
+                                            player2.setGold(input);
+                                        }
+                                    }
+                                    
+                                }
+                                break;
+                            case 3:
+                                cout << "Editing Effect. Enter the name of an effect as a string" << endl;
+                                cin >> input_string;
+                                player2.setEffect(input_string);
+                                break;
+                            case 4:
+                                if(availableCandy.size() != 0){
+                                    cout << "Adding candy. Enter one of the following options:" << endl;
+                                    displayCandyVector(availableCandy);
+                                    getline(cin, input_string);
+                                    for(int i = 0; i < availableCandy.size(); i++){
+                                        if(valid){
+                                            continue;
+                                        } else{
+                                            if(availableCandy.at(i).name == lower(input_string)){
+                                                valid = true;
+                                                player2.addCandy(availableCandy.at(i));
+                                            }
+                                        }
+                                    }
+                                } else{
+                                    cout << "Cannot add candy! Read a candy file first" << endl;
+                                }
+                                break;
+                            case 5:
+                                cout << "Removing Candy. Enter the name of a candy in player 2's inventory:" << endl;
+                                player2.printInventory();
+                                getline(cin, input_string);
+                                if(!player2.removeCandy(input_string)){
+                                    loop = true;
+                                    while(loop){
+                                        cout << "Invalid input!" << endl;
+                                        getline(cin, input_string);
+                                        if(player2.removeCandy(input_string)){
+                                            loop = false;
+                                        }
+                                    }
+                                }
+                                break;
+                            default:
+                                cout << "Invalid input!" << endl;
+                                break;
+                        }
+                        break;
+                    default:
+                        cout << "Invalid input!" << endl;
+                        break;
+                    valid = false;
+                    cout << "Select from the following options:" << endl;
+                    cout << "1. Load Candy" << endl;
+                    cout << "2. Display player stats" << endl;
+                    cout << "3. Edit player stats" << endl;
+                    cout << "4. play rock-paper-scissors" << endl;
+                    cout << "5. Exit" << endl;
+                }
+                cout << "Select from the following options:" << endl;
+                cout << "1. Load Candy" << endl;
+                cout << "2. Display player stats" << endl;
+                cout << "3. Edit player stats" << endl;
+                cout << "4. play rock-paper-scissors" << endl;
+                cout << "5. Exit" << endl;
+                input = 0;
+                break;
+            case 4:
+                break;
+            case 5:
+                running = false;
+                break;
+            default:
+                cout << "Invalid input!" << endl;
+                cout << "Select from the following options:" << endl;
+                cout << "1. Load Candy" << endl;
+                cout << "2. Display player stats" << endl;
+                cout << "3. Edit player stats" << endl;
+                cout << "4. play rock-paper-scissors" << endl;
+                cout << "5. Exit" << endl;
+                break;
+        }
+    }
 
 }
